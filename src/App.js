7bf49524214a64ff3a76f990de893e32f3b9c8b4
harshv9988 <harshv9988@gmail.com>
2020-10-18 12:30:33 +0530

    added firebase setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Cart from './Cart';
 import Navbar from './Navbar';
-import CartItem from './CartItem';
+// import CartItem from './CartItem';
 
 class App extends React.Component {
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as firebase from 'firebase'
+import 'firebase/firestore';
+
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAHaN5-9CPI4cfnZFw6ylqiUFjWNZM_E-8",
+  authDomain: "cart-978d6.firebaseapp.com",
+  databaseURL: "https://cart-978d6.firebaseio.com",
+  projectId: "cart-978d6",
+  storageBucket: "cart-978d6.appspot.com",
+  messagingSenderId: "854414615466",
+  appId: "1:854414615466:web:993392b8f2fd082ee5c1d3"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 
 ReactDOM.render(
